2397eb3e79cb52999c02e76b3d1c4dd380123362
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 01:01:09 +0430

    feat: jsx for all category added to AllCateroy component

--- src/layout/userLayout/NavBar/AllCategory/AllCategory.jsx
+++ src/layout/userLayout/NavBar/AllCategory/AllCategory.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const AllCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="px-8 py-4 bg-primary flex items-center cursor-pointer group relative">
+      <span class="text-white">
+        <i class="fas fa-bars"></i>
+      </span>
+      <span class="capitalize ml-2 text-white">All categories</span>
+    </div>
+  );
+};
 
 export default AllCategory;
--- src/layout/userLayout/NavBar/NavBar.jsx
+++ src/layout/userLayout/NavBar/NavBar.jsx
@@ -6,12 +6,7 @@ const NavBar = () => {
       <div className="container">
         <div className="flex">
           {/* all category */}
-          <div className="px-8 py-4 bg-primary flex items-center cursor-pointer group relative">
-            <span class="text-white">
-              <i class="fas fa-bars"></i>
-            </span>
-            <span class="capitalize ml-2 text-white">All categories</span>
-          </div>
+          
 
         </div>
       </div>
