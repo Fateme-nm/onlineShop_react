5e66a7ff314debeeaf97f49c2fdbd095ebef012d
fatemeh <fatemehnm1378@gmail.com>
2022-05-08 23:07:07 +0430

    feat: input for search box added to search box component

--- src/layout/userLayout/Header/components/SearchBox/SearchBox.jsx
+++ src/layout/userLayout/Header/components/SearchBox/SearchBox.jsx
@@ -6,6 +6,9 @@ const Searchbox = () => {
             <span className='absolute left-4 top-3 text-lg text-gray-400'>
                 <i className='fas fa-search'></i>
             </span>
+            <input type="text"
+                    className="pl-12 w-full border border-r-0 border-primary py-3 px-3 rounded-l-md focus:ring-primary focus:border-primary"
+                    placeholder="search"></input>
         </div>
     );
 }
