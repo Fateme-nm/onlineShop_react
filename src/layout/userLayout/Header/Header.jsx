54eab69d133bf89cc238d44da71e1e79ce2eef74
fatemeh <fatemehnm1378@gmail.com>
2022-05-08 11:19:33 +0430

    feat: add link for logo in Header of user layout

--- src/layout/userLayout/Header/Header.jsx
+++ src/layout/userLayout/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from 'routes/routes';
+import logo from 'assets/images/logo.svg'
 
 const Header = () => {
     return (
-        <header className='font-iranSans h-20'>
-            Header
+        <header className='py-4 shadow-sm bg-white'>
+            <div className='container flex items-center justify-between'>
+                <Link to={routes.HOME.path}>
+                    <img src={logo} alt="logo" className='w-32'/>
+                </Link>
+            </div>
         </header>
     );
 }
