5dceaa7ddefef83f7eccd8e013a2fa9196ee3af8
fatemeh <fatemehnm1378@gmail.com>
2022-05-08 22:42:12 +0430

    refactor: changed logo image in assets folder

--- src/layout/userLayout/Header/Header.jsx
+++ src/layout/userLayout/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from 'routes/routes';
-import logo from 'assets/images/logo.svg'
+import logo from 'assets/images/logo-daniellee.png'
 
 const Header = () => {
     return (
         <header className='py-4 shadow-sm bg-white'>
             <div className='container flex items-center justify-between'>
                 <Link to={routes.HOME.path}>
-                    <img src={logo} alt="logo" className='w-32'/>
+                    <img src={logo} alt="logo" className='w-48'/>
                 </Link>
             </div>
         </header>
