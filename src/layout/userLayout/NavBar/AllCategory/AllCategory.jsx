8afe4e063ba339ca9a86cefe3064f913e60723fb
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 01:06:38 +0430

    factory: changed name of category span in AllCategory component

--- src/layout/userLayout/NavBar/AllCategory/AllCategory.jsx
+++ src/layout/userLayout/NavBar/AllCategory/AllCategory.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const AllCategory = () => {
   return (
     <div className="px-8 py-4 bg-primary flex items-center cursor-pointer group relative">
+      <span class="capitalize mr-3 text-white">دسته بندی محصولات</span>
       <span class="text-white">
         <i class="fas fa-bars"></i>
       </span>
-      <span class="capitalize ml-2 text-white">All categories</span>
     </div>
   );
 };
