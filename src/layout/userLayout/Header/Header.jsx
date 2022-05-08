e069fba4f8c941783a8500b0d849b8823366c651
fatemeh <fatemehnm1378@gmail.com>
2022-05-08 23:49:47 +0430

    feat: import and set NavIcon components for wish, login, cart in Header component

--- src/layout/userLayout/Header/Header.jsx
+++ src/layout/userLayout/Header/Header.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from 'routes/routes';
-import logo from 'assets/images/logo-daniellee.png';
-import SearchBox from './components/SearchBox/SearchBox'
+import React from "react";
+import { Link } from "react-router-dom";
+import routes from "routes/routes";
+import logo from "assets/images/logo-daniellee.png";
+import SearchBox from "./components/SearchBox/SearchBox";
+import NavIcon from "./components/NavIcon/NavIcon";
 
 const Header = () => {
-    return (
-        <header className='py-4 shadow-sm bg-white'>
-            <div className='container flex items-center justify-between'>
-                {/* logo */}
-                <Link to={routes.HOME.path}>
-                    <img src={logo} alt="logo" className='w-48'/>
-                </Link>
-                {/* search box */}
-                <SearchBox />
-            </div>
-        </header>
-    );
-}
+  return (
+    <header className="py-4 shadow-sm bg-white">
+      <div className="container flex items-center justify-between">
+        <div className="space-x-4 flex items-center">
+          <NavIcon icon={<i class="far fa-user"></i>}>مدیریت</NavIcon>
+          <NavIcon icon={<i class="fas fa-shopping-bag"></i>} number={4}>
+            سبد خرید
+          </NavIcon>
+          <NavIcon icon={<i class="far fa-heart"></i>} number={8}>
+            ذخیره شده ها
+          </NavIcon>
+        </div>
+        {/* search box */}
+        <SearchBox />
+        {/* logo */}
+        <Link to={routes.HOME.path}>
+          <img src={logo} alt="logo" className="w-48" />
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
---
+++ src/layout/userLayout/Header/components/NavIcon/NavIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavIcon = ({children}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default NavIcon;
