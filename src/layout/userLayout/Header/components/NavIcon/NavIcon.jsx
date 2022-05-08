2821c2df2111c673ace6ac014b269baf5be715c5
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 00:04:27 +0430

    feat: icon and name and number added to NavIcon component

--- src/layout/userLayout/Header/components/NavIcon/NavIcon.jsx
+++ src/layout/userLayout/Header/components/NavIcon/NavIcon.jsx
@@ -7,7 +7,13 @@ const NavIcon = ({ icon, number, href,children }) => {
       to={href}
       class="block text-center text-gray-700 hover:text-primary transition relative"
     >
-      
+      {number ? (<span class="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
+        {number}
+      </span>) : null }
+      <div class="text-2xl">
+        {icon}
+      </div>
+      <div class="text-xs leading-3">{children}</div>
     </Link>
   );
 };
