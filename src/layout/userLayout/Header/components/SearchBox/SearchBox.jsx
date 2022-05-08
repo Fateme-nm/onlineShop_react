73c2af751aff9d28a36c3752052ab0003760ef98
fatemeh <fatemehnm1378@gmail.com>
2022-05-08 22:58:37 +0430

    feat: search icon with span tag added to search box component

--- src/layout/userLayout/Header/components/SearchBox/SearchBox.jsx
+++ src/layout/userLayout/Header/components/SearchBox/SearchBox.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Searchbox = () => {
     return (
         <div className='w-full max-w-xl relative'>
-            
+            <span className='absolute left-4 top-3 text-lg text-gray-400'>
+                <i className='fas fa-search'></i>
+            </span>
         </div>
     );
 }
