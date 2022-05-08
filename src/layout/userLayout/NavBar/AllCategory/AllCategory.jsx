0c1bc2f635a3c469f1b00c613e673f95fddf963d
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 01:20:13 +0430

    feat: NavLink component added for NavBar menu

--- src/layout/userLayout/NavBar/AllCategory/AllCategory.jsx
+++ src/layout/userLayout/NavBar/AllCategory/AllCategory.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AllCategory = () => {
   return (
     <div className="px-8 py-4 bg-primary flex items-center cursor-pointer group relative">
-      <span class="capitalize mr-3 text-white">دسته بندی محصولات</span>
+      <span class="mr-3 text-white">دسته بندی محصولات</span>
       <span class="text-white">
         <i class="fas fa-bars"></i>
       </span>
--- src/layout/userLayout/NavBar/NavBar.jsx
+++ src/layout/userLayout/NavBar/NavBar.jsx
@@ -5,10 +5,13 @@ const NavBar = () => {
   return (
     <nav className="bg-gray-800 hidden lg:block">
       <div className="container">
-        <div className="flex">
+        <div className="flex flex-row-reverse">
           {/* all category */}
           <AllCategory />
-
+          {/* nav links */}
+          <div className="flex items-center space-x-6 text-base pr-12 flex-grow">
+            
+          </div>
         </div>
       </div>
     </nav>
