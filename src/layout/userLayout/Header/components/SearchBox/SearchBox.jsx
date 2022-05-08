bd093e171676e5d1fc2c1b4a5d9eab5cb634e8da
fatemeh <fatemehnm1378@gmail.com>
2022-05-08 23:17:20 +0430

    feat: button for search box added to search box component

--- src/layout/userLayout/Header/components/SearchBox/SearchBox.jsx
+++ src/layout/userLayout/Header/components/SearchBox/SearchBox.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Searchbox = () => {
-    return (
-        <div className='w-full max-w-xl relative'>
-            <span className='absolute left-4 top-3 text-lg text-gray-400'>
-                <i className='fas fa-search'></i>
-            </span>
-            <input type="text"
-                    className="pl-12 w-full border border-r-0 border-primary py-3 px-3 rounded-l-md focus:ring-primary focus:border-primary"
-                    placeholder="search"></input>
-        </div>
-    );
-}
+  return (
+    <div className="w-full max-w-xl relative">
+      {/* search icon */}
+      <span className="absolute left-4 top-3 text-lg text-gray-400">
+        <i className="fas fa-search"></i>
+      </span>
+      {/* search input */}
+      <input
+        type="text"
+        className="pl-12 w-full border border-r-0 border-primary py-3 pr-3 rounded-l-md focus:outline-none"
+        placeholder="جستجو..."
+      ></input>
+      <button
+        type="submit"
+        className="bg-primary border border-primary text-white px-8 font-medium rounded-r-md hover:bg-transparent hover:text-primary transition"
+      >
+        <i className="fas fa-search"></i>
+      </button>
+    </div>
+  );
+};
 
 export default Searchbox;
