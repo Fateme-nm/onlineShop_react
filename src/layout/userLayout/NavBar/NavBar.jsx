620d78705a5952bc739e7fd655789b436e2dff1d
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 01:00:27 +0430

    feat: AllCategory component added for NavBar

--- src/layout/userLayout/NavBar/NavBar.jsx
+++ src/layout/userLayout/NavBar/NavBar.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const NavBar = () => {
-    return (
-        <nav className='bg-gray-800 hidden lg:block'>
-            <div className='container'>
-                <div className='flex'>
-                    
-                </div>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="bg-gray-800 hidden lg:block">
+      <div className="container">
+        <div className="flex">
+          {/* all category */}
+          <div className="px-8 py-4 bg-primary flex items-center cursor-pointer group relative">
+            <span class="text-white">
+              <i class="fas fa-bars"></i>
+            </span>
+            <span class="capitalize ml-2 text-white">All categories</span>
+          </div>
+
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
