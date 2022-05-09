35084d516215857ca75150ed7e602d50f818243d
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 17:22:17 +0430

    feat: Botton component in components folder added

--- src/pages/LoginToPanel/LoginToPanel.jsx
+++ src/pages/LoginToPanel/LoginToPanel.jsx
@@ -6,13 +6,13 @@ import InputLogin from "./InputLogin/InputLogin";
 
 const Logintopanel = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center lg:grid lg:grid-cols-2">
+    <div className="w-full h-screen flex flex-col justify-center items-center lg:grid lg:grid-cols-2">
       <img
         src={unlock}
         className="hidden lg:block w-48 hover:scale-110 transition-all duration-500 transform mx-auto"
       />
       <form className="flex flex-col justify-center items-center w-1/2">
-        <img src={avatar} className="w-32" />
+        <img src={avatar} className="w-28" />
         <h2 className="my-8 mb-0 font-bold text-3xl text-gray-700 text-center">
           خوش آمدید
         </h2>
