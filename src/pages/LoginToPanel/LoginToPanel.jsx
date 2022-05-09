d414b4db9d72afd0b032eaaacc0db966b6e5cd64
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 18:45:47 +0430

    feat: formik added to LoginToPanel and handled onSubmit

--- src/pages/LoginToPanel/LoginToPanel.jsx
+++ src/pages/LoginToPanel/LoginToPanel.jsx
@@ -6,15 +6,43 @@ import InputLogin from "./InputLogin/InputLogin";
 import { Link } from "react-router-dom";
 import routes from "routes/routes";
 import Botton from "components/Botton/Botton";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
 
 const Logintopanel = () => {
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validate,
+    onSubmit: async (values, { setSubmitting, resetForm }) => {
+      setSubmitting(false);
+      resetForm();
+      const admin = await axios
+        .get("http://localhost:3002/whoami")
+        .then(
+          (res) =>
+            res.username === formik.values.username &&
+            res.password === formik.values.password
+        )
+        .catch((err) => console.log(err));
+      admin
+        ? navigate(routes.PRODUCTS.path, { replace: true })
+        : alert("اطلاعات وارد شده نادرست است!");
+    },
+  });
   return (
     <div className="w-full h-screen flex justify-center lg:justify-between items-center px-56 flex-row-reverse">
-        <img
-          src={unlock}
-          className="hidden lg:block w-56 hover:scale-110 transition-all duration-500 transform"
-        />
-      <form className="flex flex-col justify-center items-center basis-1/2">
+      <img
+        src={unlock}
+        className="hidden lg:block w-56 hover:scale-110 transition-all duration-500 transform"
+      />
+      <form
+        className="flex flex-col justify-center items-center basis-1/2"
+        onSubmit={formik.handleSubmit}
+      >
         <img src={avatar} className="w-28" />
         <h2 className="my-8 mb-0 font-bold text-3xl text-gray-700 text-center">
           خوش آمدید
