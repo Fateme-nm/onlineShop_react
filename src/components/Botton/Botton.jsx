a8909b7486c806c3142bf7585b0285504608cd78
fatemeh <fatemehnm1378@gmail.com>
2022-05-09 17:34:20 +0430

    Button component seted for login and wraped with Link

--- src/components/Botton/Botton.jsx
+++ src/components/Botton/Botton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Botton = ({children}) => {
     return (
-        <button className="py-3 px-20 bg-primary rounded-sm text-white font-bold uppercase text-lg mt-8 transform hover:translate-y-1 transition-all duration-500 w-full">
+        <button className="py-3 px-20 bg-primary rounded-md text-white font-bold text-lg mt-8 transform hover:translate-y-1 transition-all duration-500 w-full text-center">
             {children}
         </button>
     );
--- src/pages/LoginToPanel/LoginToPanel.jsx
+++ src/pages/LoginToPanel/LoginToPanel.jsx
@@ -3,6 +3,9 @@ import WithLayoutpages from "hoc/WithLayoutPages";
 import avatar from "assets/images/undraw_male_avatar.svg";
 import unlock from "assets/images/undraw_unlock.svg";
 import InputLogin from "./InputLogin/InputLogin";
+import { Link } from "react-router-dom";
+import routes from "routes/routes";
+import Botton from "components/Botton/Botton";
 
 const Logintopanel = () => {
   return (
@@ -11,19 +14,16 @@ const Logintopanel = () => {
         src={unlock}
         className="hidden lg:block w-48 hover:scale-110 transition-all duration-500 transform mx-auto"
       />
-      <form className="flex flex-col justify-center items-center w-1/2">
+      <form className="flex flex-col justify-center items-center w-1/2 px-10">
         <img src={avatar} className="w-28" />
         <h2 className="my-8 mb-0 font-bold text-3xl text-gray-700 text-center">
           خوش آمدید
         </h2>
         <InputLogin type="text" iconClass="fa-user">نام کاربری</InputLogin>
         <InputLogin type="password" iconClass="fa-lock">رمز عبور</InputLogin>
-        <a
-          href="#"
-          
-        >
-          ورود
-        </a>
+        <Link to={routes.PRODUCTS.path} className="w-full">
+            <Botton>ورود</Botton>
+        </Link>
       </form>
     </div>
   );
