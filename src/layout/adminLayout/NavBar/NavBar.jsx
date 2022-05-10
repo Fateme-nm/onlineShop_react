1fdfcad278703f00647b3a73b42362b3ba41d36f
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 15:29:11 +0430

    feat:user navbar copied to admin navbar

--- src/layout/adminLayout/NavBar/NavBar.jsx
+++ src/layout/adminLayout/NavBar/NavBar.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const NavBar = () => {
-    return (
-        <nav>
-            HeaderAdmin
-        </nav>
-    );
-}
+  return (
+    <nav className="bg-gray-800 hidden lg:block">
+      <div className="container">
+        <div className="flex flex-row-reverse">
+          {/* all category */}
+          <AllCategory />
+          {/* nav links */}
+          <div className="flex flex-row-reverse items-center text-base pr-12 flex-grow">
+            <NavLink href={routes.HOME.path}>خانه</NavLink>
+            <NavLink href={routes.HOME.path}>درباره ما</NavLink>
+            <NavLink href={routes.HOME.path}>ارتباط با ما</NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
