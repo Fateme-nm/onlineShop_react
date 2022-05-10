f09fcb6643d5aca4476ccb3798aa7bb6b06cec44
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 15:27:15 +0430

    refactor: rename admin navbar component from Header to NavBar

--- src/layout/adminLayout/index.js
+++ src/layout/adminLayout/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header/Header';
+import Header from './NavBar/NavBar';
 import Sidebar from './SideBar/SideBar';
 
 const AdminLayout = ({children}) => {
