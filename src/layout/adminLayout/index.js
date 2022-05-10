ab7161faf5533658704b5cb80e7c55975f03aff7
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 22:16:56 +0430

    fix: layout fixed for forgot merge changes

--- src/layout/adminLayout/index.js
+++ src/layout/adminLayout/index.js
@@ -5,17 +5,8 @@ import Sidebar from './SideBar/SideBar';
 const AdminLayout = ({children}) => {
     return (
         <>
-<<<<<<< HEAD
             <NavBar />
-            <main>
-=======
-            <Header />
-<<<<<<< HEAD
             <main className='bg-gray-50 h-screen'>
->>>>>>> feat/productsManage
-=======
-            <main>
->>>>>>> feat/productsManage
                 <Sidebar />
                 {children}
             </main>
