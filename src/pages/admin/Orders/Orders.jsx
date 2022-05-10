713eabd71eea72a1844221693013545b604e2d24
fatemeh <fatemehnm1378@gmail.com>
2022-05-11 01:16:14 +0430

    feat: added Radio component for to cases in Orders component

--- src/pages/admin/Orders/Orders.jsx
+++ src/pages/admin/Orders/Orders.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Table from "./components/Table/Table";
 import WithLayoutpages from "hoc/WithLayoutPages";
 import axios from "axios";
+import Radio from "./components/Table/components/Radio";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
@@ -22,6 +23,12 @@ const Orders = () => {
     <div>
       <div className="mt-8 flex justify-between items-center flex-row-reverse container">
         <h2 className="text-2xl text-bold">مدیریت سفارش ها</h2>
+        <div className="flex justify-center">
+            <div>
+                <Radio>سفارش های تحویل شده</Radio>
+                <Radio>سفارش های در انتظار ارسال</Radio>
+            </div>
+        </div>
       </div>
       <Table orders={orders} />
     </div>
--- src/pages/admin/Orders/components/Table/components/Radio.jsx
+++ src/pages/admin/Orders/components/Table/components/Radio.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Radio = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="form-check">
+      <input
+        className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-primary checked:border-primary focus:outline-none transition duration-200 mt-1"
+        type="radio"
+        name="flexRadioDefault"
+        id="flexRadioDefault1"
+      />
+      <label
+        className="form-check-label inline-block text-gray-800"
+        htmlFor="flexRadioDefault1"
+      >
+        Default radio
+      </label>
+    </div>
+  );
+};
 
 export default Radio;
