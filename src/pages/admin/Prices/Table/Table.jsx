b51deb26369ee02e86be6d9714e32b3dcc123808
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 23:34:21 +0430

    refactor: td tags in TrTbody component updated and deleted edit and trash

--- src/pages/admin/Prices/Table/Table.jsx
+++ src/pages/admin/Prices/Table/Table.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ThThead from "./components/Th.Thead";
 import TrTbody from "./components/Tr.Tbody";
 
--- src/pages/admin/Prices/Table/components/Tr.Tbody.jsx
+++ src/pages/admin/Prices/Table/components/Tr.Tbody.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 
-const TrTbody = ({imgSrc, name, category}) => {
-    const sameClassName = "px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+const TrTbody = ({ name, price, count}) => {
+  const sameClassName = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
   return (
     <tr className="hover:bg-pink-50">
       <td className={sameClassName}>
-        <i className="fa fa-trash text-red-400 cursor-pointer text-lg"></i>
+        <div className="text-sm leading-5 text-gray-900 cursor-pointer">{count}</div>
       </td>
       <td className={sameClassName}>
-        <i class="fas fa-edit text-blue-400 cursor-pointer text-lg"></i>
-      </td>
-      <td className={sameClassName}>
-        <div className="text-sm leading-5 text-gray-900">{category}</div>
+        <div className="text-sm leading-5 text-gray-900 cursor-pointer">{price}</div>
       </td>
       <td className={sameClassName}>
         <div className="text-sm leading-5 text-gray-900">{name}</div>
-      </td>
-      <td className={`flex justify-end ${sameClassName}`}>
-        <div className="flex items-center">
-          <img
-            className="w-12 h-12"
-            src={imgSrc}
-            alt="product"
-          />
-        </div>
       </td>
     </tr>
   );
