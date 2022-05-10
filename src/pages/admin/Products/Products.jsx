635b02ba5adec5ad4a124ce1f9a4a22e8d515e2a
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 16:42:14 +0430

    className for container title and button added to admin products page

--- src/pages/admin/Products/Products.jsx
+++ src/pages/admin/Products/Products.jsx
@@ -5,8 +5,8 @@ import WithLayoutpages from "hoc/WithLayoutPages";
 const Products = () => {
   return (
     <div>
-      <div>
-        <h2>مدیریت کالا ها</h2>
+      <div className="flex justify-between items-center flex-row-reverse container">
+        <h2 className="text-xl text-bold">مدیریت کالا ها</h2>
         <button className="bg-submit rounded-md py-3 px-5 text-sm text-white font-bold transform hover:translate-y-1 transition-all duration-500 text-center">افزودن کالا</button>
       </div>
       <Table />
