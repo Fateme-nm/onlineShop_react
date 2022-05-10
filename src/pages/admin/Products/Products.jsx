06814c7c4f8b3437cb3c2185a9b6cdf008a442d5
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 17:57:00 +0430

    feat: requested to products endpoint and got it in getProducts into useEffect

--- src/pages/admin/Products/Products.jsx
+++ src/pages/admin/Products/Products.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "./components/Table/Table";
 import WithLayoutpages from "hoc/WithLayoutPages";
+import axios from "axios";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    const response = await axios
+      .get("http://localhost:3002/products")
+      .then((res) => res.data)
+      .catch((err) => console.log(err));
+    setProducts(response)
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
-      <div className="flex justify-between items-center flex-row-reverse container">
+      <div className="mt-8 flex justify-between items-center flex-row-reverse container">
         <h2 className="text-xl text-bold">مدیریت کالا ها</h2>
-        <button className="bg-submit rounded-md py-3 px-5 text-sm text-white font-bold transform hover:translate-y-1 transition-all duration-500 text-center">افزودن کالا</button>
+        <button className="bg-submit rounded-md py-3 px-5 text-sm text-white font-bold transform hover:translate-y-1 transition-all duration-500 text-center">
+          افزودن کالا
+        </button>
       </div>
       <Table />
     </div>
