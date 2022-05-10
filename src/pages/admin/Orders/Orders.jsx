ab292434c3826f3e13acdf418b1b1dc09afc5929
fatemeh <fatemehnm1378@gmail.com>
2022-05-11 00:55:45 +0430

    feat:requested to orders endpoint and get orders into getOrders function

--- src/pages/admin/Orders/Orders.jsx
+++ src/pages/admin/Orders/Orders.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import WithLayoutpages from 'hoc/WithLayoutPages';
+import React, { useEffect, useState } from "react";
+import Table from "./components/Table/Table";
+import WithLayoutpages from "hoc/WithLayoutPages";
+import axios from "axios";
 
 const Orders = () => {
-    return (
-        <div>
-            Orders
-        </div>
-    );
-}
+  const [orders, setOrders] = useState([]);
 
-export default WithLayoutpages(Orders, 'admin');
+  const getOrders = async () => {
+    const response = await axios
+      .get("http://localhost:3002/orders")
+      .then((res) => res.data)
+      .catch((err) => console.log(err));
+    setOrders(response);
+  };
+  
+  useEffect(() => {
+    getOrders()
+  }, []);
+  
+  return (
+    
+  );
+};
+
+export default WithLayoutpages(Orders, "admin");
