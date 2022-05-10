6a545f0fa28f4604ceb8f6aa5565cf6eac9ef46e
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 23:53:51 +0430

    refactor: div tag into td changed to input with att disable for count,price

--- src/pages/admin/Prices/Table/components/Tr.Tbody.jsx
+++ src/pages/admin/Prices/Table/components/Tr.Tbody.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TrTbody = ({ name, price, count}) => {
+const TrTbody = ({ name, price, count }) => {
   const sameClassName = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
+
   return (
     <tr className="hover:bg-pink-50">
-      <td className={sameClassName}>
-        <div className="text-sm leading-5 text-gray-900 cursor-pointer">{count}</div>
+      <td className={sameClassName} onClick={handleClick}>
+        <input
+          type="text"
+          disabled={disCount}
+          value={count}
+          name="count"
+          className="text-sm leading-5 text-gray-900 cursor-pointer"
+        />
       </td>
-      <td className={sameClassName}>
-        <div className="text-sm leading-5 text-gray-900 cursor-pointer">{price}</div>
+      <td className={sameClassName} onClick={handleClick}>
+        <input
+          type="text"
+          disabled={disPrice}
+          value={price}
+          name="price"
+          className="text-sm leading-5 text-gray-900 cursor-pointer"
+        />
       </td>
       <td className={sameClassName}>
         <div className="text-sm leading-5 text-gray-900">{name}</div>
