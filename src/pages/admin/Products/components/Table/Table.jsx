feb3f1082c4c4108149f7dc00086a13a66c56dd0
fatemeh <fatemehnm1378@gmail.com>
2022-05-10 14:56:41 +0430

    feat:td tags for TrTbody completed and imported to Table component

--- src/pages/admin/Products/components/Table/Table.jsx
+++ src/pages/admin/Products/components/Table/Table.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ThThead from "./components/Th.Thead";
+import TrTbody from "./components/Tr.Tbody";
 
 const Table = () => {
   return (
@@ -17,33 +18,11 @@ const Table = () => {
               </tr>
             </thead>
             <tbody className="bg-white">
-              <tr className="hover:bg-gray-50">
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <i className="fa fa-trash text-red-400 cursor-pointer"></i>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <i class="fas fa-edit text-blue-400 cursor-pointer"></i>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="text-sm leading-5 text-gray-500">
-                    john@example.com
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="text-sm leading-5 text-gray-500">
-                    john@example.com
-                  </div>
-                </td>
-                <td className="flex justify-end px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="flex items-center">
-                    <img
-                      className="w-12 h-12"
-                      src="https://source.unsplash.com/user/erondu"
-                      alt="admin dashboard ui"
-                    />
-                  </div>
-                </td>
-              </tr>
+              <TrTbody
+                imgSrc={"https://source.unsplash.com/user/erondu"}
+                name={"name"}
+                category={"category"}
+              />
             </tbody>
           </table>
         </div>
--- src/pages/admin/Products/components/Table/components/Tr.Tbody.jsx
+++ src/pages/admin/Products/components/Table/components/Tr.Tbody.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const TrTbody = () => {
-    return (
-        <div>
-            
+const TrTbody = ({imgSrc, name, category}) => {
+    const sameClassName = "px-6 py-4 whitespace-no-wrap border-b border-gray-200"
+  return (
+    <tr className="hover:bg-gray-50">
+      <td className={sameClassName}>
+        <i className="fa fa-trash text-red-400 cursor-pointer"></i>
+      </td>
+      <td className={sameClassName}>
+        <i class="fas fa-edit text-blue-400 cursor-pointer"></i>
+      </td>
+      <td className={sameClassName}>
+        <div className="text-sm leading-5 text-gray-500">{category}</div>
+      </td>
+      <td className={sameClassName}>
+        <div className="text-sm leading-5 text-gray-500">{name}</div>
+      </td>
+      <td className={`flex justify-end ${sameClassName}`}>
+        <div className="flex items-center">
+          <img
+            className="w-12 h-12"
+            src={imgSrc}
+            alt="product"
+          />
         </div>
-    );
-}
+      </td>
+    </tr>
+  );
+};
 
 export default TrTbody;
