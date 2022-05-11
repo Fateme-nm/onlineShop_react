0bb5f4c1f750ec7e858a5aaff760e31d4adaf290
fatemeh <fatemehnm1378@gmail.com>
2022-05-11 21:47:59 +0430

    feat: copied pageination from order table

--- src/pages/admin/Prices/Prices.jsx
+++ src/pages/admin/Prices/Prices.jsx
@@ -20,7 +20,7 @@ const Prices = () => {
 
   return (
     <div>
-      <div className="mt-8 flex justify-between items-center flex-row-reverse container">
+      <div className="pt-8 flex justify-between items-center flex-row-reverse container">
         <h2 className="text-2xl text-bold">مدیریت موجودی و قیمت ها</h2>
         <button className="bg-primary rounded-md py-3 px-5 text-sm text-white font-bold transform hover:translate-y-1 transition-all duration-500 text-center">
           ذخیره
--- src/pages/admin/Prices/Table/Table.jsx
+++ src/pages/admin/Prices/Table/Table.jsx
@@ -1,12 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ThThead from "./components/Th.Thead";
 import TrTbody from "./components/Tr.Tbody";
+import ReactPaginate from "react-paginate";
 
 const Table = ({ products }) => {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const pricesPerPage = 4;
+  const pagesVisited = pageNumber * pricesPerPage;
 
   return (
     <div className="flex flex-col mt-8 container">
-      <div className="py-2 -my-2">
+      <div className="py-2 -my-2 flex flex-col justify-center items-center space-y-8">
         <div className="inline-block w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
           <table className="w-full text-right">
             <thead>
@@ -17,19 +22,29 @@ const Table = ({ products }) => {
               </tr>
             </thead>
             <tbody className="bg-white">
-              {products.map((product) => {
-                return (
-                  <TrTbody
-                    name={product.name}
-                    price={product.price}
-                    count={product.count}
-                    key={product.id}
-                  />
-                );
-              })}
+              {products
+                .slice(pagesVisited, pagesVisited + pricesPerPage)
+                .map((product) => {
+                  return (
+                    <TrTbody
+                      name={product.name}
+                      price={product.price}
+                      count={product.count}
+                      key={product.id}
+                    />
+                  );
+                })}
             </tbody>
           </table>
         </div>
+        <ReactPaginate
+          previousLabel={<i className="	fa fa-chevron-left text-primary"></i>}
+          nextLabel={<i className="	fa fa-chevron-right text-primary"></i>}
+          pageCount={Math.ceil(products.length / pricesPerPage)}
+          onPageChange={({ selected }) => setPageNumber(selected)}
+          containerClassName={"flex space-x-5"}
+          activeClassName={"text-primary"}
+        />
       </div>
     </div>
   );
