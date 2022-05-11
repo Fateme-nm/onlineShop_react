db9ab46e913da40b35da1fd87f615cb98c092454
fatemeh <fatemehnm1378@gmail.com>
2022-05-11 22:24:29 +0430

    style: changed padding in ThThead

--- src/pages/admin/Prices/Table/components/Th.Thead.jsx
+++ src/pages/admin/Prices/Table/components/Th.Thead.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ThThead = ({ children }) => {
   return (
-    <th className="px-6 py-3 text-sm font-medium leading-4 tracking-wider  border-b border-primary text-gray-900 bg-white">
+    <th className="px-6 py-4 text-sm font-medium leading-4 tracking-wider  border-b border-primary text-gray-900 bg-white">
       {children}
     </th>
   );
