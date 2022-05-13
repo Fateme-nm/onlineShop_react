b32167004b120bb2a3f690cb7db8b737ac5b36f0
fatemeh <fatemehnm1378@gmail.com>
2022-05-14 00:49:37 +0430

    refactor: get admin from local storag insteade of token

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,4 +1,7 @@
 export default function authHeader() {
-    const token = localStorage.getItem("token")
-    return token ? { token } : {}
+    const admin = localStorage.getItem("admin")
+    if (admin && admin.token) {
+        return { token: admin.token }
+    }
+    return {}
 }
