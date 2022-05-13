6108235b1d5bef193e4f00a59c2911747a3432cb
fatemeh <fatemehnm1378@gmail.com>
2022-05-14 01:03:58 +0430

    feat: initailState completed with condition for admin logged in or not

--- src/store/slices/auth.js
+++ src/store/slices/auth.js
@@ -25,32 +25,10 @@ export const logout = createAsyncThunk(
     }
 );
 
-const initialState = admin
-  ? { isLoggedIn: true, admin }
-  : { isLoggedIn: false, admin: null };
-const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  extraReducers: {
-    [register.fulfilled]: (state, action) => {
-      state.isLoggedIn = false;
-    },
-    [register.rejected]: (state, action) => {
-      state.isLoggedIn = false;
-    },
-    [login.fulfilled]: (state, action) => {
-      state.isLoggedIn = true;
-      state.user = action.payload.user;
-    },
-    [login.rejected]: (state, action) => {
-      state.isLoggedIn = false;
-      state.user = null;
-    },
-    [logout.fulfilled]: (state, action) => {
-      state.isLoggedIn = false;
-      state.user = null;
-    },
-  },
-});
+const initialState = admin ? 
+    { isLoggedIn: true, isLoading: false, admin } : 
+    { isLoggedIn: false, isLoading: true, admin: null };
+
+
 const { reducer } = authSlice;
 export default reducer;
