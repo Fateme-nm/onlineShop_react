7ebb705b28f37ce3175ecdd60600bddfce8f4869
fatemeh <fatemehnm1378@gmail.com>
2022-05-14 20:38:23 +0430

    feat: added products reducer to store reducer object

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './slices/auth';
 import messageReducer from './slices/message';
 import ordersReducer from './slices/orders'
+import productsReducer from './slices/products'
 
 const reducer = {
     auth: authReducer,
     message: messageReducer,
-    orders: ordersReducer
+    orders: ordersReducer,
+    products: productsReducer
 }
 
 export const store = configureStore({
