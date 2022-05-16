ad99ba2008c6e1d7d90bda625a4f13e7d473140d
fatemeh <fatemehnm1378@gmail.com>
2022-05-16 23:27:18 +0430

    feat: imported and added removeId reducer to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './slices/auth';
 import messageReducer from './slices/message';
-import ordersReducer from './slices/orders'
-import productsReducer from './slices/products'
+import ordersReducer from './slices/orders';
+import productsReducer from './slices/products';
+import removeIdReducer from './slices/removeId'
 
 const reducer = {
     auth: authReducer,
     message: messageReducer,
     orders: ordersReducer,
-    products: productsReducer
+    products: productsReducer,
+    removeId: removeIdReducer
 }
 
 export const store = configureStore({
