d65aa9651696209556662439f244e835b081e4d5
fatemeh <fatemehnm1378@gmail.com>
2022-05-17 00:18:34 +0430

    feat: warning icon added to modal

--- src/pages/admin/Products/components/Modal/Modal.jsx
+++ src/pages/admin/Products/components/Modal/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = () => {
   const { id } = useSelector((state) => state.removeId);
 
   const handleOKClick = () => {
-    dispatch(deleteProduct(id))
+    dispatch(deleteProduct(id));
     dispatch(clearId());
   };
   const handleCancelClick = () => {
@@ -15,24 +15,25 @@ const Modal = () => {
   };
 
   return (
-    <div className="   bg-zinc-200 opacity-80 fixed inset-0 z-50   ">
-      <div className="flex h-screen justify-center items-center ">
-        <div className="flex-col justify-center  bg-white py-12 px-24 border-4 border-sky-500 rounded-xl ">
-          <div className="flex  text-lg  text-zinc-600   mb-10">
-            Are you sure ?
+    <div className="bg-zinc-200 opacity-80 fixed inset-0 z-50">
+      <div className="flex h-screen justify-center items-center">
+        <div className="flex-col justify-center bg-white py-12 px-24 border-4 border-primary rounded-xl">
+          <div className="flex text-lg text-zinc-800 mb-6 flex-col items-center">
+            <i className="fas fa-exclamation-circle mb-6 text-5xl text-gray-500"></i>
+            <p>از حذف این کالا مطمئن هستید؟</p>
           </div>
-          <div className="flex">
-            <button
-              onClick={handleOKClick}
-              className=" rounded px-4 py-2 text-white  bg-green-400 "
-            >
-              Yes
-            </button>
+          <div className="flex justify-between">
             <button
               onClick={handleCancelClick}
-              className="rounded px-4 py-2 ml-4 text-white bg-blue-500 "
+              className="rounded px-4 py-2 ml-4 text-zinc-800 bg-gray-200"
             >
-              No
+              خیر
+            </button>
+            <button
+              onClick={handleOKClick}
+              className=" rounded px-4 py-2 text-white  bg-primary"
+            >
+              بله حذف شود
             </button>
           </div>
         </div>
