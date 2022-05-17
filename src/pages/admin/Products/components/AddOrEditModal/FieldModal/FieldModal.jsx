1698656268bdb9b2a97d5a0a8d3b01e5502b3018
fatemeh <fatemehnm1378@gmail.com>
2022-05-17 10:25:09 +0430

    feat: added label tag and input tag to FieldModal

--- src/pages/admin/Products/components/AddOrEditModal/FieldModal/FieldModal.jsx
+++ src/pages/admin/Products/components/AddOrEditModal/FieldModal/FieldModal.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-const InputLogin = ({ children, type, iconClass, formik, name }) => {
+const FieldModal = ({ label, type, formik, name, id }) => {
   return (
     <div className="relative mt-8 w-full">
-      <i className={`fa ${iconClass} absolute text-primary text-xl`}></i>
+      <label htmlFor={id} className="mb-2">
+        {label}
+      </label>
       <input
         type={type}
         name={name}
-        placeholder={children}
+        id={id}
         value={formik.values[name]}
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
         className="pl-8 border-b-2 focus:outline-none focus:border-primary transition-all duration-500 text-lg w-full"
       />
       {formik.errors[name] && formik.touched[name] ? (
-             <div className="text-primary">{formik.errors[name]}</div>
-           ) : null}
+        <div className="text-primary">{formik.errors[name]}</div>
+      ) : null}
     </div>
   );
 };
 
-export default InputLogin;
+export default FieldModal;
