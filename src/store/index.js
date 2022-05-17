a98430c03c03b71f34621bc653eaa4d5c5cc6c8b
fatemeh <fatemehnm1378@gmail.com>
2022-05-17 17:54:43 +0430

    feat: added editId reducer to store

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,16 @@ import authReducer from './slices/auth';
 import messageReducer from './slices/message';
 import ordersReducer from './slices/orders';
 import productsReducer from './slices/products';
-import removeIdReducer from './slices/removeId'
+import removeIdReducer from './slices/removeId';
+import editIdReducer from './slices/EditId'
 
 const reducer = {
     auth: authReducer,
     message: messageReducer,
     orders: ordersReducer,
     products: productsReducer,
-    removeId: removeIdReducer
+    removeId: removeIdReducer,
+    editId: editIdReducer
 }
 
 export const store = configureStore({
