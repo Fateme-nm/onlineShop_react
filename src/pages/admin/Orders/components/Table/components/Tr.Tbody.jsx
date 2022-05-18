f268bf4c8cb973681008b96050aad26f92ee8e58
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 12:43:24 +0430

    feat: imported seCheckId from checkId and used useDispatch

--- src/pages/admin/Orders/components/Table/components/Tr.Tbody.jsx
+++ src/pages/admin/Orders/components/Table/components/Tr.Tbody.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setCheckId } from "store/slices/checkId";
 
 const TrTbody = ({ fullName, purchaseTotal, orderDate }) => {
   const sameClassName = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
+  const dispatch = useDispatch
+
   return (
     <tr className="hover:bg-pink-50">
       <td className={`${sameClassName} flex justify-end`}>
-        <div className="bg-opacity-80 text-xs leading-5 text-gray-50 bg-primary rounded-full w-fit px-3 py-1 cursor-pointer font-bold">بررسی سفارش</div>
+        <div className="bg-opacity-80 text-xs leading-5 text-gray-50 bg-primary rounded-full w-fit px-3 py-1 cursor-pointer font-bold"
+        onClick={}>بررسی سفارش</div>
       </td>
       <td className={sameClassName}>
         <div className="text-sm leading-5 text-gray-900">{orderDate}</div>
