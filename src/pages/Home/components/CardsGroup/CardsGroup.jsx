0d83e3f4c9dc4e23469291a115ce9da08f015e74
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 21:44:45 +0430

    feat: added title props for header of CardsGroup

--- src/pages/Home/components/CardsGroup/CardsGroup.jsx
+++ src/pages/Home/components/CardsGroup/CardsGroup.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const CardsGroup = () => {
-    return (
-        <div>
-            CardsGroup
-        </div>
-    );
-}
+const CardsGroup = ({title}) => {
+  return (
+    <div className="container pb-16">
+      <h2 class="text-2xl md:text-3xl font-medium text-gray-800 uppercase mb-6">
+        {title}
+      </h2>
+    </div>
+  );
+};
 
 export default CardsGroup;
