9915089ca7f36e3f85d808cef8de46c8006df4ec
fatemeh <fatemehnm1378@gmail.com>
2022-05-19 00:55:13 +0430

    feat: added icons facebook, twitter, instagram, linkedin to Footer

--- src/layout/userLayout/Footer/Footer.jsx
+++ src/layout/userLayout/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div class="container">
           <div class="flex justify-between flex-row-reverse items-center">
             <div className="w-1/2">
-              <p>شرکت تولیدی دنیلی</p>
+              <p>فروشگاه اینترنتی دنیلی، بررسی، انتخاب و خرید آنلاین</p>
               <p class="text-gray-500 text-base mt-3">
                 یک خرید اینترنتی مطمئن، نیازمند فروشگاهی است که بتواند کالاهایی
                 متنوع، باکیفیت و دارای قیمت مناسب را در مدت زمانی کوتاه به دست
@@ -16,7 +16,12 @@ const Footer = () => {
                 توانسته از این طریق مشتریان ثابت خود را داشته باشد.
               </p>
             </div>
-            
+            <div class="flex space-y-4 flex-col">
+              <i class="fab fa-facebook-f text-2xl text-gray-400 hover:text-gray-500 cursor-pointer"></i>
+              <i class="fab fa-twitter text-2xl text-gray-400 hover:text-gray-500 cursor-pointer"></i>
+              <i class="fab fa-instagram text-2xl text-gray-400 hover:text-gray-500 cursor-pointer"></i>
+              <i class="fab fa-linkedin-in text-2xl text-gray-400 hover:text-gray-500 cursor-pointer"></i>
+            </div>
           </div>
         </div>
       </footer>
