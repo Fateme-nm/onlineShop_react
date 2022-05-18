0a66b67940eab91b8ad672d9deee8c02db44a806
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 21:32:20 +0430

    refactor: added Banner component into home components and cut banner codes to this

---
+++ src/pages/Home/components/Banner/Banner.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import banner from 'assets/images/banner.webp'
+
+const Banner = () => {
+  return (
+    <div
+      className="bg-cover bg-no-repeat bg-center py-24 relative"
+      style={{ backgroundImage: `url(${banner})` }}
+    >
+      <div className="container">
+        <h1 className="xl:text-3xl md:text-2xl text-2xl text-gray-800 font-medium mb-4">
+          مجموعه ای از بهترین کفش ها <br class="hidden sm:block" /> برای بانوان
+        </h1>
+        <p className="text-base text-gray-600 leading-6">
+          دنیلی، سبکی برای داستان هر فرد
+        </p>
+        <div className="mt-12">
+          <a
+            href="shop.html"
+            className="bg-primary border border-primary text-white px-8 py-3 font-medium rounded-md hover:bg-transparent
+               hover:text-primary transition"
+          >
+            همین حالا خرید کنید
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
