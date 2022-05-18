bee1f220e8fb660fd638d9e66eeed39fa21ab836
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 21:33:12 +0430

    feat: imported and added Banner component into Home Component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import WithLayoutpages from "hoc/WithLayoutPages";
-
+import Banner from "./components/Banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      
+      <Banner />
     </div>
   );
 };
