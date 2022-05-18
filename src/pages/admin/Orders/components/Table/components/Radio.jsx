aacb5902e2a86f6ecd1a26c99154aae475ff2656
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 17:28:31 +0430

    refactor: changed id and htmlFor to status in Radio

--- src/pages/admin/Orders/components/Table/components/Radio.jsx
+++ src/pages/admin/Orders/components/Table/components/Radio.jsx
@@ -5,26 +5,18 @@ import { setActiveStatus } from 'store/slices/orders'
 const Radio = ({ children, status }) => {
   const dispatch = useDispatch()
 
-  const handleClick = (e) => {
-    if (e.target.id !== "all") {
-      dispatch(setActiveStatus(e.target.id))
-    }else {
-      dispatch(setActiveStatus(null))
-    }
-  }
-
   return (
     <div className="form-check flex space-x-2 items-center">
       <input
         className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-pink-100 checked:border-primary checked:border-4 focus:outline-none transition duration-200 mt-1"
         type="radio"
         name="statuses"
-        id={status ? status : "all"}
-        onClick={handleClick}
+        id={status}
+        onClick={(e) => dispatch(setActiveStatus(e.target.id))}
       />
       <label
         className="form-check-label inline-block text-gray-800"
-        htmlFor={status ? status : "all"}
+        htmlFor={status}
       >
         {children}
       </label>
