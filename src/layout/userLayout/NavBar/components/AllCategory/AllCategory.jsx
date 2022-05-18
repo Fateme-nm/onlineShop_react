64b8dbb8b2b09ba4254d12ca787cd83c3bca3dae
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 19:48:39 +0430

    feat: get categories using useSelector from products in AllCategory

--- src/layout/userLayout/NavBar/components/AllCategory/AllCategory.jsx
+++ src/layout/userLayout/NavBar/components/AllCategory/AllCategory.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const AllCategory = () => {
+  const { categories } = useSelector((state) => state.products);
+
   return (
     <div className="px-8 py-4 bg-primary flex items-center cursor-pointer group relative">
       <span className="mr-3 text-white">دسته بندی محصولات</span>
       <span className="text-white">
         <i className="fas fa-bars"></i>
       </span>
+      <div class="absolute left-0 top-full w-full bg-white shadow-md py-3 invisible opacity-0 group-hover:opacity-100 group-hover:visible transition duration-300 z-50 divide-y divide-gray-300 divide-dashed">
+        {}
+      </div>
     </div>
   );
 };
