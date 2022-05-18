299501d3c37137844624b715544245b2430ed3dc
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 22:03:47 +0430

    refactor: changed tag container Card to Link component and sat id state for this

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom";
 
 const Card = ({ imgSrc, name, price, id }) => {
   return (
-    <div class="rounded bg-white shadow overflow-hidden">
-      <div class="relative">
-        <img src={`https://localhost:3002${imgSrc}`} class="w-full" />
+    <Link
+      to={`/product/${name}`}
+      state={{ id }}
+      className="rounded bg-white shadow overflow-hidden"
+    >
+      <div className="relative">
+        <img src={`https://localhost:3002${imgSrc}`} className="w-full" />
       </div>
-      <div class="pt-4 pb-3 px-4">
-        <h4 class="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
+      <div className="pt-4 pb-3 px-4">
+        <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
           {name}
         </h4>
-        <div class="flex items-baseline mb-1 space-x-2">
-          <p class="text-xl text-primary font-roboto font-semibold">{price}</p>
+        <div className="flex items-baseline mb-1 space-x-2">
+          <p className="text-xl text-primary font-roboto font-semibold">
+            {price}
+          </p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
