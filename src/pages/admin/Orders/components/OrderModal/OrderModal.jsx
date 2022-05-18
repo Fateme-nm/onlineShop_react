30e684256cb66f5a0e9511bcceac7f633f2db931
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 13:06:56 +0430

    refactor: removed fields and form tag from copied modal in OrderModal

--- src/pages/admin/Orders/components/OrderModal/OrderModal.jsx
+++ src/pages/admin/Orders/components/OrderModal/OrderModal.jsx
@@ -12,73 +12,9 @@ const OrderModal = () => {
             <p>افزودن/ویرایش کالا</p>
           </div>
           <div className="flex justify-between">
-            <form
-              className="flex flex-col items-end w-full"
-              onSubmit={formik.handleSubmit}
-            >
-              <div className="w-full flex justify-between space-x-3">
-                {/* <FieldModal
-                  label="تصویر کالا"
-                  type="file"
-                  accept="image/*"
-                  id="image"
-                  name="image"
-                  formik={formik}
-                  input={true}
-                  imageRef={imageRef}
-                /> */}
-                <FieldModal
-                  label="نام کالا"
-                  type="text"
-                  id="name"
-                  name="name"
-                  formik={formik}
-                  input={true}
-                />
-              </div>
-              <div className="w-full flex justify-between space-x-3">
-                <FieldModal
-                  label="قیمت"
-                  type="text"
-                  id="price"
-                  name="price"
-                  formik={formik}
-                  input={true}
-                />
-                <FieldModal
-                  label="تعداد"
-                  type="number"
-                  id="count"
-                  name="count"
-                  formik={formik}
-                  input={true}
-                />
-                <FieldModal
-                  label="دسته بندی"
-                  id="categoryId"
-                  name="categoryId"
-                  formik={formik}
-                  select={true}
-                />
-                <FieldModal
-                  label="رنگ"
-                  id="colorId"
-                  name="colorId"
-                  formik={formik}
-                  select={true}
-                />
-              </div>
-              <FieldModal
-                label="توضیحات"
-                id="description"
-                name="description"
-                formik={formik}
-                textarea={true}
-              />
               <button type="submit" className="bg-submit rounded-md py-2 px-4">
                 ذخیره
               </button>
-            </form>
           </div>
         </div>
       </div>
