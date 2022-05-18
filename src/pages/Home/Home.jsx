b47461785ac7eb4fc8e24f046e7839f2ed9c6887
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 21:40:58 +0430

    feat: imported and added CardsGroup component into Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import WithLayoutpages from "hoc/WithLayoutPages";
 import Banner from "./components/Banner/Banner";
+import CardsGroup from "./components/CardsGroup/CardsGroup";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <CardsGroup />
     </div>
   );
 };
