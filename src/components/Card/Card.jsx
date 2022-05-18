744fee98c65242e2c512be84bd8eb93e19868d60
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 21:53:41 +0430

    feat: added imgSrc, name, price and id as props to Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Card = () => {
-    return (
-        <div>
-            card
-        </div>
-    );
-}
+const Card = ({imgSrc, name, price, id}) => {
+  return (
+    <div class="rounded bg-white shadow overflow-hidden">
+      <div class="relative">
+        <img src={`https://localhost:3002${imgSrc}`} class="w-full" />
+        <div class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+          
+    </div>
+  );
+};
 
 export default Card;
