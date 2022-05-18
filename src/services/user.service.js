10d7fe1b16169b58ec24c1e94ad30ae5ba539548
fatemeh <fatemehnm1378@gmail.com>
2022-05-19 02:34:10 +0430

    feat: added user.service.js to services

---
+++ src/services/user.service.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+const API_URL = "http://localhost:3002/";
+
+const getProducts = () => {
+  return axios.get(API_URL + "products", { headers: authHeader() });
+};
+
+const getCategoreis = () => {
+  return axios.get(API_URL + "category", { headers: authHeader() });
+};
+
+const getColors = () => {
+  return axios.get(API_URL + "color", { headers: authHeader() });
+}
+
+const getOrders = () => {
+  return axios.get(API_URL + "orders", { headers: authHeader() });
+};
+
+const getStatusOrders = () => {
+    return axios.get(API_URL + "orderStatus", { headers: authHeader() })
+}
+
+const postProduct = (formData) => {
+  return axios.post(API_URL + "products", formData, 
+    { headers: authHeader(), "Content-Type": "multipart/form-data"})
+}
+
+const updateProduct = (formData, id) => {
+  return axios.patch(API_URL + `products/${id}`, formData, 
+    { headers: authHeader(), "Content-Type": "multipart/form-data"})
+}
+
+const updateOrder = (deliveredAt, id) => {
+  return axios.patch(API_URL + `orders/${id}`, deliveredAt, { headers: authHeader() })
+}
+
+const deleteProduct = (id) => {
+  return axios.delete(API_URL + `products/${id}`, { headers: authHeader() })
+}
+
+const adminService = {
+    getProducts,
+    getCategoreis,
+    getColors,
+    getOrders,
+    getStatusOrders,
+    postProduct,
+    updateProduct,
+    updateOrder,
+    deleteProduct
+};
+
+export default adminService
