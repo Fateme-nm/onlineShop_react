3b5d6a32aff1cef76c0e14ff5e1f6e0dd80a7d39
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 15:46:12 +0430

    feat: added orders state from orders reducer as dipendency useEffect and dispatch getOrders init

--- src/pages/admin/Orders/Orders.jsx
+++ src/pages/admin/Orders/Orders.jsx
@@ -8,13 +8,13 @@ import OrderModal from "./components/OrderModal/OrderModal";
 
 const Orders = () => {
   const dispatch = useDispatch();
-  const { activeStatus, activeSort } = useSelector((state) => state.orders);
+  const { activeStatus, activeSort, orders } = useSelector((state) => state.orders);
   const { check_id } = useSelector((state) => state.checkId);
   const [checkModalOn, setCheckModalOn] = useState(false);
 
   useEffect(() => {
     dispatch(getOrders());
-  }, []);
+  }, [orders]);
 
   useEffect(() => {
     setCheckModalOn(check_id ? true : false)
