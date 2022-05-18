f81fee06266049c7b5f36d16248cabedbcdc768a
fatemeh <fatemehnm1378@gmail.com>
2022-05-18 12:44:53 +0430

    feat: dispatched to setCheckId with id parameter when onClick event

--- src/pages/admin/Orders/components/Table/components/Tr.Tbody.jsx
+++ src/pages/admin/Orders/components/Table/components/Tr.Tbody.jsx
@@ -4,13 +4,13 @@ import { setCheckId } from "store/slices/checkId";
 
 const TrTbody = ({ fullName, purchaseTotal, orderDate }) => {
   const sameClassName = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
-  const dispatch = useDispatch
+  const dispatch = useDispatch()
 
   return (
     <tr className="hover:bg-pink-50">
       <td className={`${sameClassName} flex justify-end`}>
         <div className="bg-opacity-80 text-xs leading-5 text-gray-50 bg-primary rounded-full w-fit px-3 py-1 cursor-pointer font-bold"
-        onClick={}>بررسی سفارش</div>
+        onClick={() => dispatch(setCheckId(id))}>بررسی سفارش</div>
       </td>
       <td className={sameClassName}>
         <div className="text-sm leading-5 text-gray-900">{orderDate}</div>
