555827e41dd776befa0a05f67c0f48ee27572f74
fatemeh <fatemehnm1378@gmail.com>
2022-05-19 23:18:08 +0430

    feat: added HttpServie.js into services

---
+++ src/services/HttpService.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+class HttpService {
+    constructor() {
+        axios.defaults.baseURL="http://localhost:3002/";
+        axios.defaults.timeout=2000;
+        axios.interceptors.request.use(config => {
+            if (config.url === "orders") {
+                config.headers["token"] = localStorage.getItem("token")
+            }
+            return config
+        }, error => {
+            return Promise.reject(error)
+        })
+        axios.interceptors.response.use(response => {
+            return response
+        }, error => {
+            Promise.reject(error)
+        })
+    }
+
+    get = async(url) => {
+        try {
+            const response = await axios.get(url)
+            return response
+        } catch (error) {
+            return error
+        }
+    }
+
+    post = async(url, data) => {
+        try {
+            const response = await axios.post(url, data)
+            return response.data
+        } catch (error) {
+            return error
+        }
+    }
+
+    patch = async(url, data) => {
+        try {
+            const response = await axios.patch(url, data)
+            return response.data
+        } catch (error) {
+            return error
+        }
+    }
+
+    delete = async(url, data) => {
+        try {
+            const response = await axios.delete(url, data)
+            return response.data
+        } catch (error) {
+            return error
+        }
+    }
+}
+
+export default HttpService
