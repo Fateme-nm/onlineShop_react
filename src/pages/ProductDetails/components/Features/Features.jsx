2c99da78613de2c1dc870bdc90843481e06b87e5
fatemeh <fatemehnm1378@gmail.com>
2022-05-19 21:50:16 +0430

    feat: added quantity to Features.jsx

--- src/pages/ProductDetails/components/Features/Features.jsx
+++ src/pages/ProductDetails/components/Features/Features.jsx
@@ -33,18 +33,18 @@ const Features = ({product}) => {
         <h3 className="text-gray-800 mb-1">رنگ</h3>
         <button className="text-xs border border-gray-200 rounded-sm h-6 w-6 shadow-sm bg-red-600">{colorId}</button>
       </div>
-      {/* <div class="mt-4 flex justify-between items-center flex-row-reverse">
-        <h3 class="text-gray-800 mb-1">تعداد</h3>
-        <div class="flex border border-gray-300 text-gray-600 divide-x divide-gray-300">
-          <button class="h-8 w-8 text-xl flex items-center justify-center cursor-pointer">
+      <div className="mt-4 flex justify-between items-center flex-row-reverse">
+        <h3 className="text-gray-800 mb-1">تعداد</h3>
+        <div className="flex border border-gray-300 text-gray-600 divide-x divide-gray-300">
+          <button className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer">
             -
           </button>
-          <div class="h-8 w-10 flex items-center justify-center">1</div>
-          <button class="h-8 w-8 text-xl flex items-center justify-center cursor-pointer">
+          <div className="h-8 w-10 flex items-center justify-center">1</div>
+          <button className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer">
             +
           </button>
         </div>
-      </div> */}
+      </div>
     </>
   );
 };
