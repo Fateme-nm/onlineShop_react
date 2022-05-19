a880dc63f4b7a5ad938d801b53d26881a00547af
fatemeh <fatemehnm1378@gmail.com>
2022-05-19 21:57:51 +0430

    feat: added onClick for quantity buttons in Features

--- src/pages/ProductDetails/components/Features/Features.jsx
+++ src/pages/ProductDetails/components/Features/Features.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-const Features = ({product}) => {
-    const {name, categoryId, price, sizes, colorId} = product
+const Features = ({ product }) => {
+  const { name, categoryId, price, sizes, colorId } = product;
   return (
     <>
-      <h2 className="md:text-3xl text-2xl font-medium mb-4">
-        {name}
-      </h2>
+      <h2 className="md:text-3xl text-2xl font-medium mb-4">{name}</h2>
       <div className="space-x-2 flex flex-row-reverse">
         <span className="text-gray-800">: دسته بندی</span>
         <span className="text-gray-600">{categoryId}</span>
@@ -31,16 +29,18 @@ const Features = ({product}) => {
       </div>
       <div className="mt-4 flex justify-between items-center flex-row-reverse">
         <h3 className="text-gray-800 mb-1">رنگ</h3>
-        <button className="text-xs border border-gray-200 rounded-sm h-6 w-6 shadow-sm bg-red-600">{colorId}</button>
+        <button className="text-xs border border-gray-200 rounded-sm h-6 w-6 shadow-sm bg-red-600">
+          {colorId}
+        </button>
       </div>
       <div className="mt-4 flex justify-between items-center flex-row-reverse">
         <h3 className="text-gray-800 mb-1">تعداد</h3>
         <div className="flex border border-gray-300 text-gray-600 divide-x divide-gray-300">
-          <button className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer">
+          <button className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer" onClick={() => handleClickQuntity(1)}>
             -
           </button>
           <div className="h-8 w-10 flex items-center justify-center">1</div>
-          <button className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer">
+          <button className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer" onClick={() => handleClickQuntity(-1)}>
             +
           </button>
         </div>
