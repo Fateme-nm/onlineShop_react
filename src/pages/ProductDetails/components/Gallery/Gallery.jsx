d9bf0425db956c15e0a15a8369600b412d9ea953
fatemeh <fatemehnm1378@gmail.com>
2022-05-19 13:33:51 +0430

    feat: maped images and sat src with values in Gallery

--- src/pages/ProductDetails/components/Gallery/Gallery.jsx
+++ src/pages/ProductDetails/components/Gallery/Gallery.jsx
@@ -11,24 +11,16 @@ const Gallery = (mainImg, images) => {
         />
       </div>
       <div className="grid grid-cols-5 gap-4 mt-4">
-        <div>
-          <img
-            src="./image"
-            className="w-full cursor-pointer border border-primary"
-          />
-        </div>
-        <div>
-          <img src="./image" className="w-full cursor-pointer border" />
-        </div>
-        <div>
-          <img src="./image" className="w-full cursor-pointer border" />
-        </div>
-        <div>
-          <img src="./image" className="w-full cursor-pointer border" />
-        </div>
-        <div>
-          <img src="./image" className="w-full cursor-pointer border" />
-        </div>
+        {images.map((img) => {
+          return (
+            <div>
+              <img
+                src={`https://localhost3002${img}`}
+                className="w-full cursor-pointer border border-primary"
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
