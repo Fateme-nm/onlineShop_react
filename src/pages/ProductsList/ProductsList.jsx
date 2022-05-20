0e0bde05c517bfc7b64fef8effc4e63f333854eb
fatemeh <fatemehnm1378@gmail.com>
2022-05-20 10:38:06 +0430

    feat: imported and sat Sidebar component in ProductsList

--- src/pages/ProductsList/ProductsList.jsx
+++ src/pages/ProductsList/ProductsList.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import WithLayoutpages from "hoc/WithLayoutPages";
+import Sidebar from "./components/Sidebar";
 
 const Productslist = () => {
   return (
     <div className="container grid lg:grid-cols-4 gap-6 pt-4 pb-16 items-start relative">
-      
+      <Sidebar />
     </div>
   );
 };
