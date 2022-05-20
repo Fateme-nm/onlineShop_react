60262b29b66d884dc862abdfaf913a2d6b573ab9
fatemeh <fatemehnm1378@gmail.com>
2022-05-20 11:51:00 +0430

    feat: added pagination for products in ProductsList

--- src/pages/ProductsList/ProductsList.jsx
+++ src/pages/ProductsList/ProductsList.jsx
@@ -4,9 +4,14 @@ import Sidebar from "./components/Sidebar";
 import Filter from "./components/Filter/Filter";
 import Card from "components/Card/Card";
 import httpService from "services/HttpService";
+import ReactPaginate from "react-paginate";
 
 const Productslist = () => {
   const [products, setProducts] = useState([])
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const productsPerPage = 6;
+  const pagesVisited = pageNumber * productsPerPage;
 
   const handleRequestProducts = () => {
       await httpService
@@ -21,6 +26,14 @@ const Productslist = () => {
     <div className="container grid lg:grid-cols-4 gap-6 pt-4 pb-16 items-start relative">
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 sm:grid-cols-2 gap-6">
         {/* <Card /> */}
+        <ReactPaginate
+            previousLabel={<i className="	fa fa-chevron-left text-primary"></i>}
+            nextLabel={<i className="	fa fa-chevron-right text-primary"></i>}
+            pageCount={Math.ceil(showProducts.length / productsPerPage)}
+            onPageChange={({ selected }) => setPageNumber(selected)}
+            containerClassName={"flex space-x-5"}
+            activeClassName={"text-primary"}
+          />
       </div>
       <Filter />
       <Sidebar />
