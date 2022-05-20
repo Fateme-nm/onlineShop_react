582a3602bb92bcc9a420532853df54c0cf72319e
fatemeh <fatemehnm1378@gmail.com>
2022-05-20 10:51:40 +0430

    feat: imported and sat Card into ProductsList

--- src/pages/ProductsList/ProductsList.jsx
+++ src/pages/ProductsList/ProductsList.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import WithLayoutpages from "hoc/WithLayoutPages";
 import Sidebar from "./components/Sidebar";
 import Filter from "./components/Filter/Filter";
+import Card from "components/Card/Card";
 
 const Productslist = () => {
   return (
     <div className="container grid lg:grid-cols-4 gap-6 pt-4 pb-16 items-start relative">
       <Sidebar />
       <Filter />
+      <div className="grid lg:grid-cols-2 xl:grid-cols-3 sm:grid-cols-2 gap-6">
+        <Card />
+      </div>
     </div>
   );
 };
