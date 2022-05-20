183cef1146e30c5568ecb73ea99e0b7ac142d414
fatemeh <fatemehnm1378@gmail.com>
2022-05-20 11:39:15 +0430

    feat: get proudcts into handleRequestProducts in ProductsList

--- src/pages/ProductsList/ProductsList.jsx
+++ src/pages/ProductsList/ProductsList.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WithLayoutpages from "hoc/WithLayoutPages";
 import Sidebar from "./components/Sidebar";
 import Filter from "./components/Filter/Filter";
 import Card from "components/Card/Card";
+import httpService from "services/HttpService";
 
 const Productslist = () => {
+  const [products, setProducts] = useState([])
+
+  const handleRequestProducts = () => {
+      await httpService
+        .get('products')
+        .then(res => setProducts(res.data.reverse()))
+  };
+
+  useEffect(() => {
+    handleRequestProducts();
+  }, []);
   return (
     <div className="container grid lg:grid-cols-4 gap-6 pt-4 pb-16 items-start relative">
-      <Sidebar />
-      <Filter />
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 sm:grid-cols-2 gap-6">
-        <Card />
+        {/* <Card /> */}
       </div>
+      <Filter />
+      <Sidebar />
     </div>
   );
 };
