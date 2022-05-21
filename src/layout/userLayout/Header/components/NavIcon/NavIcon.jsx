878c8ca6d87bc78469cb6baebb6b52b48617b671
fatemeh <fatemehnm1378@gmail.com>
2022-05-21 19:45:59 +0430

    refactor: changed top, right and width, height of cart icon number in NavIcon

--- src/layout/userLayout/Header/components/NavIcon/NavIcon.jsx
+++ src/layout/userLayout/Header/components/NavIcon/NavIcon.jsx
@@ -7,7 +7,7 @@ const NavIcon = ({ icon, number, href,children }) => {
       to={href}
       class="text-center text-gray-700 hover:text-primary transition relative"
     >
-      {number ? (<span class="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
+      {number ? (<span class="absolute right-1 top-0 w-4 h-4 rounded-full flex items-center justify-center bg-primary text-white text-xs">
         {number}
       </span>) : null }
       <div class="text-2xl text-center">
