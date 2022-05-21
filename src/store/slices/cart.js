7fec52e608a4a705a4e30722fa3074879c948afb
fatemeh <fatemehnm1378@gmail.com>
2022-05-21 18:22:58 +0430

    feat: added cartSlice in store/slices folder for store cart detials

---
+++ src/store/slices/cart.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+  },
+});
+
+const { reducer, actions } = cartSlice;
+export const { } = actions
+export default reducer;
