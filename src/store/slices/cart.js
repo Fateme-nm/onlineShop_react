6cdecf3670ec19ae993c31352a06ab3410b2d590
fatemeh <fatemehnm1378@gmail.com>
2022-05-21 19:06:21 +0430

    feat: added cartProducts state and push new product in handleAddToCart action in cart slice

--- src/store/slices/cart.js
+++ src/store/slices/cart.js
@@ -1,14 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+    cartProducts: []
+};
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    handleAddToCart: (state, action) => {
+        state.cartProducts.push(action.payload)
+    }
   },
 });
 
 const { reducer, actions } = cartSlice;
-export const { } = actions
+export const { handleAddToCart } = actions
 export default reducer;
