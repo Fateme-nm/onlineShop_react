71fe578c177fbd8e739b353ef786b80e5d0813a8
fatemeh <fatemehnm1378@gmail.com>
2022-05-23 19:15:16 +0430

    feat: defined showPreview state in ImageUploader and show response in modal

--- src/pages/admin/Products/components/AddOrEditModal/ImageUploader/ImageUploader.jsx
+++ src/pages/admin/Products/components/AddOrEditModal/ImageUploader/ImageUploader.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import httpService from "services/HttpService";
 
 const ImageUploader = ({ name, formik, setThumbnailImg }) => {
+  const [showPreview, setShowPreview] = useState();
   const handleChangeImage = (e) => {
-    const formData = new FormData()
-    formData.append("image",e.target.files[0])
-    httpService.post("upload", formData).then((res) => {
-      setThumbnailImg(res.data);
-      console.log(res.data.filename);
-    });
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+    httpService
+      .post("upload", formData)
+      .then((res) => {
+        setThumbnailImg(res.data.filename)
+        setShowPreview(`http://localhost:3002/files/${res.data.filename}`)
+      });
   };
+
   return (
     <div>
       <div class="flex items-center justify-center w-full">
         <label class="flex flex-col w-36 h-34 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
           <div class="relative flex flex-col items-center justify-center pt-7">
-            <img id="preview" class="absolute inset-0 w-full h-32" />
+            <img src={showPreview} class="absolute inset-0 w-full h-32" />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="w-12 h-12 text-gray-400"
