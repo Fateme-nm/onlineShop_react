34c346eceb4bda844698f03c36655fe9ca30209c
fatemeh <fatemehnm1378@gmail.com>
2022-05-24 23:56:05 +0430

    refactor: changed handleSubmit for changed price and count in TrTbody

--- src/pages/admin/Prices/Table/components/Tr.Tbody.jsx
+++ src/pages/admin/Prices/Table/components/Tr.Tbody.jsx
@@ -1,47 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { updateProduct } from "store/slices/products";
 import { useDispatch } from "react-redux";
-import { persinaDigit } from "utils";
-import EasyEdit, { Types } from "";
+import { persinaDigit, separate } from "utils";
+import EasyEdit, { Types } from "react-easy-edit";
 
 const TrTbody = ({ name, price, count, id, submitChanges }) => {
   const sameClassName = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   if (submitChanges && (changeCount || changePrice)) {
-  //     if (changeCount) {
-  //       const formData = new FormData();
-  //       formData.append("count", changeCount);
-  //       formData.append("id", id);
-  //       dispatch(updateProduct(formData));
-  //       setDisCount(true);
-  //     }
-  //     if (changePrice) {
-  //       const formData = new FormData();
-  //       formData.append("price", changePrice);
-  //       formData.append("id", id);
-  //       dispatch(updateProduct(formData));
-  //       setDisPrice(true);
-  //     }
-  //   }
-  // }, [submitChanges]);
+  const handleSubmit = (key, value) => {
+    const formData = new FormData()
+    formData.append("id", id)
+    formData.append(key, value)
+    dispatch(updateProduct(formData))
+  }
 
   return (
     <tr className="hover:bg-pink-50">
       <td className={sameClassName}>
         <EasyEdit
-          type={Types.TEXT}
-          value={count}
+          type={Types.NUMBER}
+          value={persinaDigit(count)}
           name="count"
+          onSave={value => handleSubmit("count", value)}
           className="text-sm leading-5 text-gray-900 cursor-pointer rounded-md p-2 bg-transparent"
         />
       </td>
       <td className={sameClassName}>
         <EasyEdit
-          type={Types.TEXT}
-          value={price}
+          type={Types.NUMBER}
+          value={persinaDigit(separate(price))}
           name="price"
+          onSave={value => handleSubmit("price", value)}
           className="text-sm leading-5 text-gray-900 cursor-pointer rounded-md p-2 bg-transparent"
         />
       </td>
