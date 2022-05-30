ac49954c3667512281b828722caea4bc19250009
fatemeh <fatemehnm1378@gmail.com>
2022-05-30 18:33:39 +0430

    feat: added continue button in OrderSummary

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import WithLayoutpages from "hoc/WithLayoutPages";
 import CartItem from "./CartItem/CartItem";
 import { getShowCartProducts } from "store/slices/cart";
 import { useDispatch, useSelector } from "react-redux";
+import OrderSummary from "./OrderSummary/OrderSummary";
 
 const Cart = () => {
   const { cartProducts, showCartProducts } = useSelector((state) => state.cart);
@@ -16,7 +17,7 @@ const Cart = () => {
       <div class="flex shadow-md my-10 flex-row-reverse">
         <div class="w-3/4 bg-white px-10 py-10">
           <div class="flex justify-between border-b pb-8 flex-row-reverse">
-            <h2 class="font-semibold text-2xl">سبد خرید شما</h2>
+            <h2 class="font-semibold text-xl">سبد خرید شما</h2>
             <div className="flex items-center flex-row-reverse space-x-2 space-x-reverse cursor-pointer">
               <h3>حذف همه</h3>
               <i className="fa fa-trash text-primary"></i>
@@ -44,47 +45,7 @@ const Cart = () => {
             ))}
           </ul>
         </div>
-        <div id="summary" class="w-1/4 px-8 py-10">
-          <h1 class="font-semibold text-2xl border-b pb-8">Order Summary</h1>
-          <div class="flex justify-between mt-10 mb-5">
-            <span class="font-semibold text-sm uppercase">Items 3</span>
-            <span class="font-semibold text-sm">590$</span>
-          </div>
-          <div>
-            <label class="font-medium inline-block mb-3 text-sm uppercase">
-              Shipping
-            </label>
-            <select class="block p-2 text-gray-600 w-full text-sm">
-              <option>Standard shipping - $10.00</option>
-            </select>
-          </div>
-          <div class="py-10">
-            <label
-              for="promo"
-              class="font-semibold inline-block mb-3 text-sm uppercase"
-            >
-              Promo Code
-            </label>
-            <input
-              type="text"
-              id="promo"
-              placeholder="Enter your code"
-              class="p-2 text-sm w-full"
-            />
-          </div>
-          <button class="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">
-            Apply
-          </button>
-          <div class="border-t mt-8">
-            <div class="flex font-semibold justify-between py-6 text-sm uppercase">
-              <span>Total cost</span>
-              <span>$600</span>
-            </div>
-            <button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
-              Checkout
-            </button>
-          </div>
-        </div>
+        <OrderSummary />
       </div>
     </div>
   );
---
+++ src/pages/Cart/OrderSummary/OrderSummary.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { persinaDigit, separate } from "utils";
+
+const OrderSummary = () => {
+  return (
+    <div class="w-1/4 px-8 py-10 border-r border-gray-200">
+      <div class="flex justify-between mb-5 flex-row-reverse">
+        <span class="font-semibold text-sm">تعداد کالا ها</span>
+        <span class="font-semibold text-sm">{persinaDigit(2)}</span>
+      </div>
+      <div class="flex justify-between mb-5 flex-row-reverse">
+        <span class="font-semibold text-sm">جمع سبد خرید</span>
+        <span class="font-semibold text-sm">{persinaDigit(separate(2))}</span>
+      </div>
+      <div>
+        <p className="text-sm text-gray-500">
+          هزینه این سفارش هنوز پرداخت نشده‌ و در صورت اتمام موجودی، کالاها از
+          سبد حذف می‌شوند
+        </p>
+      </div>
+      <div class="border-t mt-8">
+        <button class="bg-primary font-semibold hover:bg-transparent hover:text-primary border border-primary transition-all py-3 text-sm text-white uppercase w-full mt-8 text-center rounded-md">
+          نهایی کردن سبد خرید
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSummary;
