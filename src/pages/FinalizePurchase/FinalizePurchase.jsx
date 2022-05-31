32889d68b0b40d180ca0db0d55c927863731e2be
fatemeh <fatemehnm1378@gmail.com>
2022-05-31 19:10:24 +0430

    feat: used Field component for form tags in FinalizePurchase

--- src/pages/FinalizePurchase/FinalizePurchase.jsx
+++ src/pages/FinalizePurchase/FinalizePurchase.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import WithLayoutpages from "hoc/WithLayoutPages";
 import PaymentSummary from "./components/PaymentSummary";
 import { useFormik } from "formik";
+import * as Yup from "yup";
+import Field from "./components/Field/Field";
+
+const validationSchema = Yup.object().shape({});
 
 const Finalizepurchase = () => {
-    const formik = useFormik({
-        initialValues: {
-            lastname: "",
-            firstname: "",
-            address: "",
-            phone: "", 
-            date: ""
-        },
-        
-    })
+  const formik = useFormik({
+    initialValues: {
+      lastname: "",
+      firstname: "",
+      address: "",
+      phone: "",
+      date: "",
+    },
+    onSubmit: async (values, { resetForm }) => {},
+    validationSchema,
+  });
   return (
     <div className="container mx-auto mt-10">
       <div className="flex shadow-md my-10 flex-row-reverse">
@@ -21,7 +26,42 @@ const Finalizepurchase = () => {
           <div className="flex justify-between border-b pb-8 flex-row-reverse">
             <h2 className="font-semibold text-xl">نهایی کردن خرید</h2>
           </div>
-          
+          <div>
+            <form>
+              <Field
+                label="نام"
+                type="text"
+                id="firstname"
+                name="firstname"
+                formik={formik}
+                input={true}
+              />
+              <Field
+                label="نام خانوادگی"
+                type="text"
+                id="lastname"
+                name="lastname"
+                formik={formik}
+                input={true}
+              />
+              <Field
+                label="آدرس"
+                type="text"
+                id="lastname"
+                name="lastname"
+                formik={formik}
+                textarea={true}
+              />
+              <Field
+                label="نام"
+                type="phone"
+                id="phone"
+                name="phone"
+                formik={formik}
+                input={true}
+              />
+            </form>
+          </div>
         </div>
         <PaymentSummary />
       </div>
---
+++ src/pages/FinalizePurchase/components/Field/Field.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Field = ({ label, type, formik, name, id, input, textarea }) => {
+  return (
+    <div className="w-full">
+      <label htmlFor={id} className="mb-2">
+        {label}
+      </label>
+      {input && (
+        <input
+          type={type}
+          name={name}
+          id={id}
+          value={formik.values[name]}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          className="w-full border border-gray-300 focus:border-primary focus:outline-0 rounded-sm h-8"
+        />
+      )}
+      {textarea && (
+        <textarea
+          name={name}
+          id={id}
+          cols="30"
+          rows="10"
+          value={formik.values[name]}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          className="w-full border border-gray-300 focus:border-primary focus:outline-0 rounded-sm h-8"
+        ></textarea>
+      )}
+      {formik.errors[name] && formik.touched[name] ? (
+        <div className="text-primary">{formik.errors[name]}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Field;
