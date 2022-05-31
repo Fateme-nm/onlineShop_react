839e314c97549686355c0f88d24f93b700a626e9
fatemeh <fatemehnm1378@gmail.com>
2022-05-31 18:31:11 +0430

    feat: added useFormik to FinalizePurchase

--- src/pages/FinalizePurchase/FinalizePurchase.jsx
+++ src/pages/FinalizePurchase/FinalizePurchase.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import WithLayoutpages from 'hoc/WithLayoutPages';
+import React from "react";
+import WithLayoutpages from "hoc/WithLayoutPages";
+import PaymentSummary from "./components/PaymentSummary";
+import { useFormik } from "formik";
 
 const Finalizepurchase = () => {
-    return (
-        <div>
-            FinalizePurchase
+    const formik = useFormik({
+        initialValues: {
+            lastname: "",
+            firstname: "",
+            address: "",
+            phone: "", 
+            date: ""
+        },
+        
+    })
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="flex shadow-md my-10 flex-row-reverse">
+        <div className="w-3/4 bg-white px-10 py-10">
+          <div className="flex justify-between border-b pb-8 flex-row-reverse">
+            <h2 className="font-semibold text-xl">نهایی کردن خرید</h2>
+          </div>
+          
         </div>
-    );
-}
+        <PaymentSummary />
+      </div>
+    </div>
+  );
+};
 
 export default WithLayoutpages(Finalizepurchase);
