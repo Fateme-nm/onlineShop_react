36185257d8bd41cdc3199860b4c6699c0e2ac599
fatemeh <fatemehnm1378@gmail.com>
2022-06-02 14:52:51 +0430

    feat: added handleCancle and navigat to payment result with successful state with flase value

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -48,28 +48,34 @@ const Payment = () => {
       );
   };
 
-  const handleCancle = () => {};
-  if (showCartProducts) {
-    return (
-      <div className="bg-gray-100">
-        <img src={paymentImg} />
-        <div className="flex flex-row-reverse justify-center space-x-5 space-x-reverse py-8">
-          <button
-            className="bg-submit py-2 px-20 rounded-full text-gray-50"
-            onClick={handlePayment}
-          >
-            پرداخت
-          </button>
-          <button
-            className="bg-amber-400 py-2 px-10 rounded-full text-gray-50"
-            onClick={handleCancle}
-          >
-            انصراف
-          </button>
-        </div>
-      </div>
-    );
-  }
+  const handleCancle = () => {
+    navigate(routes.PAYMENT_RESULT.path, {
+      replace: true,
+      state: { successful: false },
+    });
+  };
+  
+  //   if (showCartProducts) {
+  //     return (
+  //       <div className="bg-gray-100">
+  //         <img src={paymentImg} />
+  //         <div className="flex flex-row-reverse justify-center space-x-5 space-x-reverse py-8">
+  //           <button
+  //             className="bg-submit py-2 px-20 rounded-full text-gray-50"
+  //             onClick={handlePayment}
+  //           >
+  //             پرداخت
+  //           </button>
+  //           <button
+  //             className="bg-amber-400 py-2 px-10 rounded-full text-gray-50"
+  //             onClick={handleCancle}
+  //           >
+  //             انصراف
+  //           </button>
+  //         </div>
+  //       </div>
+  //     );
+  //   }
 };
 
 export default Payment;
