a673e0afd88fd6c9413c8a7d710458208eecbc82
fatemeh <fatemehnm1378@gmail.com>
2022-06-02 11:53:47 +0430

    feat: added payment image and two button for Payment page

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import WithLayoutpages from 'hoc/WithLayoutPages';
+import paymentImg from 'assets/images/paymentImg.png'
 
 const Payment = () => {
     return (
-        <div>
-            Payment
+        <div className='bg-gray-100'>
+            <img src={paymentImg} />
+            <div className='flex flex-row-reverse justify-center space-x-5 space-x-reverse py-8'>
+                <button>پرداخت</button>
+                <button>انصراف</button>
+            </div>
         </div>
     );
 }
 
-export default WithLayoutpages(Payment);
+export default Payment;
