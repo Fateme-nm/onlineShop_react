38e1d9f369d365bebd3a7e76feee6c83655e79b4
fatemeh <fatemehnm1378@gmail.com>
2022-06-02 12:15:28 +0430

    feat: added styles to buttons in Payment

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import paymentImg from 'assets/images/paymentImg.png'
+import React from "react";
+import paymentImg from "assets/images/paymentImg.png";
 
 const Payment = () => {
-    return (
-        <div className='bg-gray-100'>
-            <img src={paymentImg} />
-            <div className='flex flex-row-reverse justify-center space-x-5 space-x-reverse py-8'>
-                <button>پرداخت</button>
-                <button>انصراف</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="bg-gray-100">
+      <img src={paymentImg} />
+      <div className="flex flex-row-reverse justify-center space-x-5 space-x-reverse py-8">
+        <button className="bg-submit py-2 px-20 rounded-full text-gray-50">
+          پرداخت
+        </button>
+        <button className="bg-amber-400 py-2 px-10 rounded-full text-gray-50">
+          انصراف
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Payment;
