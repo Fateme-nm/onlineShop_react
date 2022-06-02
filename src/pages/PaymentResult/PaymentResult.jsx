396ec9a00adf52d28b356c9a239e7cf930aba140
fatemeh <fatemehnm1378@gmail.com>
2022-06-02 17:29:41 +0430

    refactor: removed payment component and transform codes into PaymentResult

--- src/pages/PaymentResult/PaymentResult.jsx
+++ src/pages/PaymentResult/PaymentResult.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import WithLayoutpages from 'hoc/WithLayoutPages';
+import React from "react";
+import WithLayoutpages from "hoc/WithLayoutPages";
+import React, { useEffect } from "react";
+import paymentImg from "assets/images/paymentImg.png";
+import { useLocation, useNavigate } from "react-router-dom";
+import httpService from "services/HttpService";
+import { useDispatch, useSelector } from "react-redux";
+import { getShowCartProducts, handleSyncStorage } from "store/slices/cart";
+import routes from "routes/routes";
 
 const Paymentresult = () => {
-    return (
-        <div>
-            PaymentResult
-        </div>
-    );
-}
+  const { showCartProducts, cartProducts } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(handleSyncStorage());
+  }, []);
+
+  useEffect(() => {
+    dispatch(getShowCartProducts());
+  }, [cartProducts]);
+
+  const handlePayment = () => {
+    httpService
+      .post("orders", {
+        customerDetail: { lastName, firstName, phone, billingAddress },
+        purchaseTotal: showCartProducts.totalPrice,
+        orderStatus: 3,
+        orderDate: new Date().getTime(),
+        delivery: date,
+        deliveredAt: null,
+        orderItems: showCartProducts.products.map((pro) => {
+          const { name, image, price, quantity } = pro;
+          return { name, thumbnail: image, price, quantity };
+        }),
+      })
+      .then(() =>
+        navigate(routes.PAYMENT_RESULT.path, {
+          replace: true,
+          state: { successful: true },
+        })
+      );
+  };
+
+  const handleCancle = () => {
+    navigate(routes.PAYMENT_RESULT.path, {
+      replace: true,
+      state: { successful: false },
+    });
+  };
+  return <div>PaymentResult</div>;
+};
 
 export default WithLayoutpages(Paymentresult);
